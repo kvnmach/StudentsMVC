5de5750b0e65e2fbc07ade1c1349669c93a0235c
kvnmach <kvnmach@gmail.com>
2016-04-24 12:07:25 -0500

    Added files via upload

---
+++ Students/Controllers/StudentsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Students.Models;
+
+namespace Students.Controllers
+{
+    public class StudentsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            
+            var presentStudents = (List<Student>)Session["students"] ?? new List<Student>();
+
+            Session["students"] = presentStudents;
+            return View(presentStudents);
+
+
+
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student newStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                var presentStudents = (List<Student>) Session["students"];
+                if (presentStudents == null)
+                {
+                    presentStudents = new List<Student>();
+                }
+                presentStudents.Add(newStudent);
+                Session["students"] = presentStudents;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            List<Student> Students = new List<Student>();
+            var editId = Students.FirstOrDefault(s => s.Id == s.Id);
+            return View(editId);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            return View();
+        }
+    }
+}
---
+++ Students/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Student()
+        {
+            
+        }
+
+        public Student(int id, string firstName, string lastName, string gender, int age)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Age = age;
+        }
+    }
+}
